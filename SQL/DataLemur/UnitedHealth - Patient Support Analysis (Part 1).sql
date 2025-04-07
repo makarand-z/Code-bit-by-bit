d52dff5d3af57dc8119d27035e6701360665e0af
Makarand Zende <zende.r.makarand@gmail.com>
2025-04-07 10:01:45 -0400

    Added solution using a CTE

--- SQL/DataLemur/UnitedHealth - Patient Support Analysis (Part 1).sql
+++ SQL/DataLemur/UnitedHealth - Patient Support Analysis (Part 1).sql
@@ -48,3 +48,16 @@ FROM (
   GROUP BY policy_holder_id
   HAVING COUNT(case_id) >= 3
 ) AS records;
+
+-- using CTE 
+
+WITH call_records AS (
+	SELECT policy_holder_id,
+	COUNT(case_id) AS call_count
+	FROM callers
+	GROUP BY policy_holder_id
+	HAVING COUNT(case_id) >= 3
+)
+
+SELECT COUNT(policy_holder_id) AS policy_holder_count
+FROM call_records;
